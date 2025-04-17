310a399d7d2c4f3b6341ee73d2b4ca166dea9e0c
Gavin Corlette <gavinccorlette@gmail.com>
2025-04-17 09:56:05 -0400

    Basic routing

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to your shift scheduler!"
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
