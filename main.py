e491bc51beb5f2c0a6e7ce9ae23f2622f8007c42
Gavin Corlette <gavinccorlette@gmail.com>
2025-04-20 09:29:43 -0400

    Some formatting logic and comments

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
 from flask import Flask, request, redirect, render_template, url_for
+from datetime import datetime
 
 app = Flask(__name__)
 
 shift_list = []
 
+# Render home page
 @app.route("/")
 def home():
     return render_template("home.html")
 
+# Render schedule page
 @app.route("/schedule")
 def schedule():
     return render_template("schedule.html", shifts=shift_list)
 
+# Add all attributes to dictionary, add dictionary to list
 @app.route("/add-shift", methods=["POST"])
 def add_shift():
     fname = (request.form.get("fname"))
     lname = (request.form.get("lname"))
     shift_date = (request.form.get("date"))
-    shift_start = (request.form.get("start"))
-    shift_end = (request.form.get("end"))
-    my_dict = {"First Name":fname, "Last Name":lname, "Date":shift_date, "Start Time":shift_start, "End Time":shift_end}
+    shift_start = datetime.strptime(request.form.get("start"), "%H:%M") # Formatting time to 12-hour format
+    shift_end = datetime.strptime(request.form.get("end"), "%H:%M")
+    my_dict = {"First Name":fname, "Last Name":lname, "Date":shift_date, "Start Time":shift_start.strftime("%I:%M %p"), "End Time":shift_end.strftime("%I:%M %p")}
     shift_list.append(my_dict)
     return redirect(url_for("schedule"))
 
-
+# Run application
 if __name__ == "__main__":
     app.run(debug=True)
 
