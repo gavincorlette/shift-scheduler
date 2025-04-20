d75367f7a210e0c1507abbacb9c64d482ac3650d
Gavin Corlette <gavinccorlette@gmail.com>
2025-04-20 10:21:46 -0400

    Preparing to allow user to edit and delete shifts

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 
 shift_list = []
 
+# ID for shifts in order to delete or edit them
+next_id = 1
+
 # Render home page
 @app.route("/")
 def home():
@@ -18,13 +21,28 @@ def schedule():
 # Add all attributes to dictionary, add dictionary to list
 @app.route("/add-shift", methods=["POST"])
 def add_shift():
+    global next_id
     fname = (request.form.get("fname"))
     lname = (request.form.get("lname"))
-    shift_date = (request.form.get("date"))
+    shift_date = datetime.strptime(request.form.get("date"), "%Y-%m-%d") # Formatting date to be more readable
     shift_start = datetime.strptime(request.form.get("start"), "%H:%M") # Formatting time to 12-hour format
     shift_end = datetime.strptime(request.form.get("end"), "%H:%M")
-    my_dict = {"First Name":fname, "Last Name":lname, "Date":shift_date, "Start Time":shift_start.strftime("%I:%M %p"), "End Time":shift_end.strftime("%I:%M %p")}
+    my_dict = {"ID":next_id, "First Name":fname, "Last Name":lname, "Date":shift_date.strftime("%b %d, %Y"), "Start Time":shift_start.strftime("%I:%M %p"), "End Time":shift_end.strftime("%I:%M %p")}
     shift_list.append(my_dict)
+    
+    # Each time a shift is submitted, the ID increments
+    next_id += 1
+
+    return redirect(url_for("schedule"))
+
+# Function to edit shift
+@app.route("/edit-shift", methods=["GET", "POST"])
+def edit_shift():
+    return redirect(url_for("schedule"))
+
+# Function to delete shift
+@app.route("/delete-shift", methods=["POST"])
+def delete_shift():
     return redirect(url_for("schedule"))
 
 # Run application
